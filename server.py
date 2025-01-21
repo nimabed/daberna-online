3eeb65fd206642b06861ed27443fad19512844f4
Nima <nima.abed.ch@gmail.com>
2025-01-21 13:56:36 +0330

    server class added

--- server.py
+++ server.py
@@ -1,126 +1,252 @@
-import socket, pickle, random, time, hashlib, struct, copy
+import socket, pickle, random, time, hashlib, struct
 from _thread import *
 from gctl import Game
 
 
-host = "192.168.1.9"
-port = 9999
-
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    server.bind((host, port))
-except socket.error as e:
-    print(f"Bindig error: {e}")
-
-def generate_card():
-    card = []
-    for i in range(9):
-        card.append(random.sample(range(1 + i*10, 11 + i*10), 3))
-    for _ in range(15):
-        card[random.randint(0,8)][random.randint(0,2)] = "*"
-    return [[str(item) for item in row]for row in card]
-
-
-def random_numbers(game, clients):
-    global numbers
-    
-    time.sleep(0.2)
-    for client in clients:
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port 
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            client.sendall(pickle.dumps(players_cards))
-        except:
-            print("Can not send players cards!")
-            return
+            self.server.bind((self.host, self.port))
+        except socket.error as e:
+            print(f"Binding error: {e}")
 
-    copy_counter = game.random_num_counter
-    while True:
-        if game.running:
-            num = random.choice(numbers)
-            game.rand_num = num
-            numbers.remove(num)
-            for i in range(copy_counter):
-                game.random_num_counter -= 1
-                if game.result[0] or game.result[1]:
-                    game.rand_num = None
-                    return
-                time.sleep(1)
-            game.random_num_counter = copy_counter
-        else:
-            game.start_counter -= 1
-            time.sleep(2)
-            if game.start_counter == 1:
-                game.running = True
+        # Variables
+        # self.player = 0
+        self.game = Game()
+        self.numbers = [i for i in range(1,91)]
+        self.players_cards = {}
+        self.clients = []
+
+    def generate_card(self):
+        card = []
+        for i in range(9):
+            card.append(random.sample(range(1 + i*10, 11 + i*10), 3))
+        for _ in range(15):
+            card[random.randint(0,8)][random.randint(0,2)] = "*"
+        return [[str(item) for item in row]for row in card]
     
+    def random_numbers(self):
+        copy_counter = self.game.random_num_counter
     
-def active_client(connection, player, game):
-   
-    while True:
-        try:
-            raw_data = connection.recv(4096)
-            if not raw_data:
-                print(f"Player {player} disconnected!")
-                break
+        time.sleep(0.2)
+        for client in self.clients:
+            try:
+                client.sendall(pickle.dumps(self.players_cards))
+            except:
+                print("Can not send players cards!")
+                return
+
+        while True:
+            if self.game.running:
+                num = random.choice(self.numbers)
+                self.game.rand_num = num
+                self.numbers.remove(num)
+                for i in range(copy_counter):
+                    self.game.random_num_counter -= 1
+                    if self.game.result[0] or self.game.result[1]:
+                        self.game.rand_num = None
+                        return
+                    time.sleep(1)
+                self.game.random_num_counter = copy_counter
             else:
-                data = raw_data.decode()
-        
-                if data != "get":
-                    game.player_move(player, data)
-                    game.winner_check(player, players_cards[player])
+                self.game.start_counter -= 1
+                time.sleep(2)
+                if self.game.start_counter == 1:
+                    self.game.running = True
 
+    def active_client(self, connection, player):
+            
+        while True:
+            try:
+                raw_data = connection.recv(4096)
+                if not raw_data:
+                    print(f"Player {player} disconnected!")
+                    break
                 else:
-                    serialized_game = game.serialize()
-                    checksum = hashlib.sha256(serialized_game).hexdigest()
-                    message_bytes = checksum.encode()+serialized_game
-                    message_length = len(message_bytes) 
-                    connection.sendall(struct.pack("I", message_length))
-                    connection.sendall(message_bytes)
-                             
-        except:
-            pass
+                    data = raw_data.decode()
+            
+                    if data != "get":
+                        self.game.player_move(player, data)
+                        self.game.winner_check(player, self.players_cards[player])
 
-    if player == 1:
-        game.p1 = False
-    else:
-        game.p2 = False
-    game.running = False
-    connection.close()
-    print("Connection close!")
+                    else:
+                        serialized_game = self.game.serialize()
+                        checksum = hashlib.sha256(serialized_game).hexdigest()
+                        message_bytes = checksum.encode()+serialized_game
+                        message_length = len(message_bytes) 
+                        connection.sendall(struct.pack("I", message_length))
+                        connection.sendall(message_bytes)
+                                
+            except:
+                pass
+        
+        if player == 1: self.game.p1 = False
+        else: self.game.p2 = False
+        # self.game.p1 = False if player == 1 else self.game.p2 = False
+        # if player == 1:
+        #     game.p1 = False
+        # else:
+        #     game.p2 = False
+        self.game.running = False
+        connection.close()
+        print("Connection close!")
+
+    def run(self):
+        player = 0
+
+        self.server.listen(2)
+        print("listening...")
+
+        while True:
+            conn, addr = self.server.accept()
+            player += 1
+            self.clients.append(conn)
+            conn.send(str(player).encode())
+            print(f"Player {player} with address {addr} added!")
+
+            try:
+                data = conn.recv(1024)
+                if not data:
+                    print("Could not receive number of cards!")
+                    break
+                else:
+                    cards_number = data.decode()
+                    self.players_cards[player] = [self.generate_card() for _ in range(int(cards_number))]
+            except:
+                pass
+
+            if player == 2:
+                self.game.p1, self.game.p2 = True, True
+                start_new_thread(self.random_numbers, ())
+
+            start_new_thread(self.active_client, (conn, player))
+
+
+if __name__ == "__main__":
+    server = Server("192.168.1.9", 9999)
+    server.run()
+
+# host = "192.168.1.9"
+# port = 9999
+
+
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# try:
+#     server.bind((host, port))
+# except socket.error as e:
+#     print(f"Bindig error: {e}")
+
+# def generate_card():
+#     card = []
+#     for i in range(9):
+#         card.append(random.sample(range(1 + i*10, 11 + i*10), 3))
+#     for _ in range(15):
+#         card[random.randint(0,8)][random.randint(0,2)] = "*"
+#     return [[str(item) for item in row]for row in card]
+
+
+# def random_numbers(game, clients):
+#     global numbers
+#     copy_counter = game.random_num_counter
+    
+#     time.sleep(0.2)
+#     for client in clients:
+#         try:
+#             client.sendall(pickle.dumps(players_cards))
+#         except:
+#             print("Can not send players cards!")
+#             return
+
+#     while True:
+#         if game.running:
+#             num = random.choice(numbers)
+#             game.rand_num = num
+#             numbers.remove(num)
+#             for i in range(copy_counter):
+#                 game.random_num_counter -= 1
+#                 if game.result[0] or game.result[1]:
+#                     game.rand_num = None
+#                     return
+#                 time.sleep(1)
+#             game.random_num_counter = copy_counter
+#         else:
+#             game.start_counter -= 1
+#             time.sleep(2)
+#             if game.start_counter == 1:
+#                 game.running = True
+    
+    
+# def active_client(connection, player, game):
+   
+#     while True:
+#         try:
+#             raw_data = connection.recv(4096)
+#             if not raw_data:
+#                 print(f"Player {player} disconnected!")
+#                 break
+#             else:
+#                 data = raw_data.decode()
+        
+#                 if data != "get":
+#                     game.player_move(player, data)
+#                     game.winner_check(player, players_cards[player])
+
+#                 else:
+#                     serialized_game = game.serialize()
+#                     checksum = hashlib.sha256(serialized_game).hexdigest()
+#                     message_bytes = checksum.encode()+serialized_game
+#                     message_length = len(message_bytes) 
+#                     connection.sendall(struct.pack("I", message_length))
+#                     connection.sendall(message_bytes)
+                             
+#         except:
+#             pass
+
+#     if player == 1:
+#         game.p1 = False
+#     else:
+#         game.p2 = False
+#     game.running = False
+#     connection.close()
+#     print("Connection close!")
     
 
-server.listen(2)
-print("listening...")
+# server.listen(2)
+# print("listening...")
 
-player = 0
-game = Game()
-players_cards = {}
-numbers = [i for i in range(1,91)]
-clients = []
+# player = 0
+# game = Game()
+# players_cards = {}
+# numbers = [i for i in range(1,91)]
+# clients = []
 
-while True:
+# while True:
 
-    conn, addr = server.accept()
-    player += 1
-    clients.append(conn)
-    conn.send(str(player).encode())
-    print(f"Player {player} with address {addr} added!")
+#     conn, addr = server.accept()
+#     player += 1
+#     clients.append(conn)
+#     conn.send(str(player).encode())
+#     print(f"Player {player} with address {addr} added!")
 
-    try:
-        data = conn.recv(1024)
-        if not data:
-            print("Could not receive number of cards!")
-            break
-        else:
-            cards_number = data.decode()
-            players_cards[player] = [generate_card() for _ in range(int(cards_number))]
+#     try:
+#         data = conn.recv(1024)
+#         if not data:
+#             print("Could not receive number of cards!")
+#             break
+#         else:
+#             cards_number = data.decode()
+#             players_cards[player] = [generate_card() for _ in range(int(cards_number))]
 
-    except:
-        pass
+#     except:
+#         pass
 
-    if player == 2:
-        game.p1, game.p2 = True, True
-        start_new_thread(random_numbers, (game, clients))
+#     if player == 2:
+#         game.p1, game.p2 = True, True
+#         start_new_thread(random_numbers, (game, clients))
 
-    start_new_thread(active_client, (conn, player, game))
+#     start_new_thread(active_client, (conn, player, game))
 
 
