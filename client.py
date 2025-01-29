8d099b16f2441c5b3568c783b734da1b24dd8084
Nima <nima.abed.ch@gmail.com>
2025-01-29 08:54:11 +0330

    draw opponent and fill rect methods added to Rects class

--- client.py
+++ client.py
@@ -6,18 +6,28 @@ pygame.init()
 clock = pygame.time.Clock()
 
 class Rects:
-    def __init__(self, x, y, width, height, text):
+    def __init__(self, x, y, width, height, text, text_size):
         self.x = x
         self.y = y
         self.text = text
         self.width = width
         self.height = height
+        self.font = pygame.font.SysFont("Lato Black", text_size)
 
-    def draw(self, win):
-        font = pygame.font.SysFont("Lato Black", 30)
+    def draw_player(self, win):
+        # font = pygame.font.SysFont("Lato Black", self.text_size)
         pygame.draw.rect(win, (0,0,255), (self.x, self.y, self.width, self.height), 2)
-        num = font.render(self.text, 1, (0,0,0))
+        num = self.font.render(self.text, 1, (0,0,0))
         win.blit(num, (self.x + self.width/2 - num.get_width()/2, self.y + self.height/2 - num.get_height()/2))
+
+    def draw_opponent(self, win):
+        # font = pygame.font.SysFont("Lato Black", self.text_size)
+        if self.text == "*":
+            pygame.draw.rect(win, (0,0,255), (self.x, self.y, self.width, self.height))
+        else:
+            pygame.draw.rect(win, (0,0,255), (self.x, self.y, self.width, self.height), 2)
+            num = self.font.render(self.text, 1, (0,0,0))
+            win.blit(num, (self.x + self.width/2 - num.get_width()/2, self.y + self.height/2 - num.get_height()/2))
     
     def clicked(self, pos):
         if (self.x <= pos[0] <= self.x + self.width) and (self.y <= pos[1]<= self.y + self.height):
