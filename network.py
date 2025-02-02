9c979edac42cdadee31772b1ca1a00fa4a075f60
Nima <nima.abed.ch@gmail.com>
2025-02-02 17:17:55 +0330

    received cards method on network file changed

--- network.py
+++ network.py
@@ -7,6 +7,7 @@ class Network:
         self.ip = ip
         self.port = port
         self.player = self.connect()
+        self.game = Game(2)
 
     def get_p_id(self):
         return int(self.player[0])
@@ -56,9 +57,9 @@ class Network:
             elif data == "get":
                 data_recv = self.received_all()
                 if data_recv and self.check_seri(data_recv):
-                    game = Game(2)
-                    game.deserialize(data_recv[64:])
-                    return game
+                    # game = Game(2)
+                    self.game.deserialize(data_recv[64:])
+                    return self.game
                 else:
                     print("Checksum mismatch!")
                     print(data_recv)
@@ -68,5 +69,20 @@ class Network:
             print(f"Sending error: {e}")
 
     def receive_cards(self):
-        return pickle.loads(self.client.recv(4096*5))
+        received_bytes = self.received_all()
+        if received_bytes and self.check_seri(received_bytes):
+            players_cards = self.game.deserialize_cards(received_bytes[64:])
+            return players_cards
+        else:
+            print("Checksum mismatch!!!!")
+            return
+        # return pickle.loads(self.client.recv(4096*5))
+        
                    
+
+# mydict = {1: [[['*', '*', '4'], ['19', '15', '16'], ['*', '21', '30'], ['32', '*', '35'], ['50', '45', '*'], ['*', '51', '58'], ['64', '70', '*'], ['73', '*', '78'], ['*', '*', '*']], [['*', '*', '10'], ['*', '11', '17'], ['29', '23', '26'], ['*', '*', '32'], ['47', '41', '45'], ['53', '*', '52'], ['*', '*', '*'], ['73', '74', '77'], ['*', '*', '*']]], 2: [[['*', '10', '4'], ['20', '13', '*'], ['*', '28', '24'], ['*', '37', '*'], ['*', '47', '48'], ['56', '*', '58'], ['67', '*', '70'], ['78', '*', '*'], ['85', '*', '90']], [['*', '*', '7'], ['18', '15', '11'], ['22', '26', '*'], ['33', '*', '*'], ['49', '*', '50'], ['57', '*', '*'], ['64', '69', '66'], ['*', '*', '76'], ['84', '*', '85']], [['6', '*', '*'], ['20', '*', '17'], ['28', '23', '*'], ['31', '34', '36'], ['*', '*', '43'], ['53', '*', '54'], ['65', '64', '*'], ['*', '79', '*'], ['90', '83', '86']]]}
+
+# net = Network("192.168.1.9", 9999)
+
+# data = net.game.serialize_cards(mydict)
+# print(net.game.deserialize_cards(data))
